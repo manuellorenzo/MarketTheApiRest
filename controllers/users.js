35092ff43891377229193586b8089212a004ddb1
Manuel <manuel.lorenzo@optimaonline.es>
2018-04-09 11:36:54 +0200

    Users CRUD done

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,63 @@ var Users = require('../models/users');
 
 //GET - Return all users in the DB
 function findAllUsers(req, res) {
-	Users.find(function(err, users) {
-    if(err) res.send(500, err.message);
-    console.log('GET /users')
-		res.status(200).jsonp(users);
-	});
+    Users.find(function (err, users) {
+        if (err) res.send(500, err.message);
+        console.log('GET /users')
+        res.status(200).jsonp(users);
+    });
 };
 
+//GET - Return a user with specified ID
+function findUserById(req, res) {
+    Users.findById(req.params.id, function (err, tvshow) {
+        if (err) return res.send(500, err.message);
+        console.log('GET /findUserById/' + req.params.id);
+        res.status(200).jsonp(tvshow);
+    });
+};
+
+function addUser(req, res) {
+    console.log('POST');
+    console.log(req.body);
+
+    var user = new Users({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email
+    });
+
+    user.save(function (err, user) {
+        if (err) return res.status(500).send(err.message);
+        res.status(200).jsonp(user);
+    });
+};
+
+function updateUser(req, res) {
+    Users.findById(req.params.id, function (err, user) {
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.email = req.body.email;
+
+        user.save(function (err) {
+            if (err) return res.status(500).send(err.message);
+            res.status(200).jsonp(user);
+        });
+    });
+};
+
+function deleteUser(req, res) {
+    Users.findById(req.params.id, function (err, user) {
+        user.remove(function (err) {
+            if (err) return res.status(500).send(err.message);
+            res.status(200).send();
+        })
+    });
+};
 module.exports = {
-    findAllUsers
+    findAllUsers,
+    findUserById,
+    addUser,
+    updateUser,
+    deleteUser
 };
--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,12 @@ var api = express.Router();
  
 // Creamos una ruta de tipo GET para el método de pruebas
 api.get('/findAllUsers', UsersController.findAllUsers);
- 
+api.get('/findUserById/:id', UsersController.findUserById);
+
+api.post('/addUser', UsersController.addUser);
+
+api.put('/updateUser/:id', UsersController.updateUser);
+
+api.delete('/deleteUser/:id', UsersController.deleteUser);
 // Exportamos la configuración
 module.exports = api;
